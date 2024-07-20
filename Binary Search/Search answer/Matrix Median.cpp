7cb48ed6c27f00a4c84633191426c4e9d126c820
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-07-20 22:01:17 +0530

    Update Matrix Median.cpp

--- Binary Search/Search answer/Matrix Median.cpp
+++ Binary Search/Search answer/Matrix Median.cpp
@@ -1,53 +1,45 @@
-int upperBound(vector<int> &A, int x, int n)
-{
-    int low = 0, high = n-1;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
     int ans = n;
-    while(low <= high)
-    {
-        int mid = (low + high)/2;
-    if(A[mid] > x) {
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
             ans = mid;
-            high = mid-1;
+            //look for smaller index on the left
+            high = mid - 1;
         }
-        else{
-            low = mid+1;
+        else {
+            low = mid + 1; // look on the right
         }
     }
     return ans;
 }
-
-int countSmallEqual(vector<vector<int>> &A, int m, int n, int x)
-{
-    int count = 0;
-    for(int i=0;i<m;i++)
-    {
-        count += upperBound(A[i],x,n);
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
     }
-    return count;
+    return cnt;
 }
 
-int Solution::findMedian(vector<vector<int> > &A) 
-{
-    int low = INT_MAX, high = INT_MIN;
-    int n = A.size();
-    int m = A[0].size();
-    for(int i=0; i<m; i++)
-    {
-        low = min(low,A[i][0]);
-        high = max(high, A[i][n-1]);
+int Solution::findMedian(vector<vector<int> > &A) {
+int low = INT_MAX, high = INT_MIN;
+ int m = A.size(), n = A[0].size();
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, A[i][0]);
+        high = max(high, A[i][n - 1]);
     }
-    int req = (n*m)/2;
-    while(low <= high)
-    {
-        int mid = (low + high)/2;
-        int small = countSmallEqual(A,m,n,mid);
-        if(small <= req)
-        {
-            low = mid+1;
-        }
-        else{
-            high = mid-1;
-        }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(A, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
     }
     return low;
 }
