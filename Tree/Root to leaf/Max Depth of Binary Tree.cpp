a6ed1c50baeeee1a94189671f3f0c1c1949005be
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-08-08 20:20:37 +0530

    Update Max Depth of Binary Tree.cpp

--- Tree/Root to leaf/Max Depth of Binary Tree.cpp
+++ Tree/Root to leaf/Max Depth of Binary Tree.cpp
@@ -1,1 +1,9 @@
+TC : O(N) 
+SC : O(N LOG N) high of binary tree  recursion stack
 
+int Solution::maxDepth(TreeNode* A) {
+    if(A == NULL) return 0;
+    int left = maxDepth(A->left);
+    int right = maxDepth(A->right);
+    return max(left,right)+1;
+}
