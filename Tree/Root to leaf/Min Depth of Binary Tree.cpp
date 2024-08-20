d15831109b789fb6c928863f188edc44cef27153
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-08-20 22:37:31 +0530

    Update Min Depth of Binary Tree.cpp

--- Tree/Root to leaf/Min Depth of Binary Tree.cpp
+++ Tree/Root to leaf/Min Depth of Binary Tree.cpp
@@ -1,1 +1,12 @@
-
+int Solution::minDepth(TreeNode* A) 
+{
+    if(A == NULL) return 0;
+    // leaf node 
+    if(A->left == NULL && A->right == NULL)
+    {
+        return 1;
+    }
+    int L = A->left!=NULL ? minDepth(A->left):INT_MAX;
+    int R = A->right!=NULL ? minDepth(A->right):INT_MAX;
+    return 1+min(L,R);
+}
