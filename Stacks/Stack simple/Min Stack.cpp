f83ea25c92a2ffb27c24f04b2ab40d178fd41062
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-06 22:35:53 +0530

    Create Min Stack.cpp

---
+++ Stacks/Stack simple/Min Stack.cpp
@@ -0,0 +1,45 @@
+TC : O(1)
+SC : O(2XN) 
+
+stack<pair<int,int>> st;
+MinStack::MinStack() {
+    st = stack<pair<int,int>> ();
+}
+
+void MinStack::push(int x) {
+    int mini;
+    if(st.empty()){
+        mini = x;
+    }
+    else{
+        mini = min(st.top().second, x);
+    }
+    st.push({x, mini});
+}
+
+void MinStack::pop() 
+{
+    if (!st.empty()) {
+            st.pop();
+        }
+}
+
+int MinStack::top() {
+  if (st.empty()) {
+            return -1; 
+        }
+        return st.top().first;
+}
+
+int MinStack::getMin() {
+       if (st.empty()) {
+            return -1;
+        }
+        return st.top().second;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Approach 2 : Optimize Space Complexity 
+
+
