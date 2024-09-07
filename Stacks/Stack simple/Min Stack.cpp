029d5366543e157458b608cb3b6853923c3c38d5
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-07 10:03:28 +0530

    Update Min Stack.cpp

--- Stacks/Stack simple/Min Stack.cpp
+++ Stacks/Stack simple/Min Stack.cpp
@@ -42,6 +42,60 @@ int MinStack::getMin() {
 
 Approach 2 : Optimize Space Complexity 
 
+TC : O(1)
+SC : O(N)
+
+stack<long long> st;
+long long mini;
+MinStack::MinStack() {
+    while(!st.empty()){
+        st.pop();
+    }
+    mini = INT_MAX;
+}
+
+void MinStack::push(int x) {
+    if(st.empty()){
+        mini = x;
+        st.push(x);
+    }
+    else{
+        if(x < mini)
+        {
+            st.push(2*x*1LL - mini);
+            mini = x;
+        }
+        else{
+            st.push(x);
+        }
+    }
+}
+
+void MinStack::pop() {
+    if(st.empty()) return;
+    long long ele = st.top();
+    st.pop();
+    if(ele < mini)
+    {
+        mini = 2*mini - ele;
+    }
+}
+
+int MinStack::top() {
+    if(st.empty()) return -1;
+long long ele = st.top();
+if(ele < mini)
+{
+    return mini;
+}
+return ele;
+}
+
+int MinStack::getMin() {
+     if (st.empty()) return -1;
+    return mini;
+}
+
 
 
 
