50020da7aeaf5d1724e45e9ea118ee24b6137567
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-07 17:00:57 +0530

    Update Rain Water Trapped.cpp

--- Stacks/Stack math/Rain Water Trapped.cpp
+++ Stacks/Stack math/Rain Water Trapped.cpp
@@ -79,4 +79,43 @@ int Solution::trap(const vector<int> &A) {
     return ans;
 }
 
-          
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Approach 3 : Optimized (Two - Pointer)
+
+TC : O(N) 
+SC : O(1)
+
+int Solution::trap(const vector<int> &A)
+{
+    int n = A.size();
+    int left = 0, right = n-1;
+    int res =0;
+    int maxLeft =0, maxRight = 0;
+    while(left <= right)
+    {
+        if(A[left] <= A[right])
+        {
+            if(A[left] >= maxLeft)
+            {
+                maxLeft = A[left];
+            }
+            else{
+                res += maxLeft - A[left];
+            }
+            left++;
+        }
+        else{
+            if(A[right] >= maxRight)
+            {
+                maxRight = A[right];
+            }
+            else{
+                res += maxRight - A[right];
+            }
+            right--;
+        }
+    }
+    return res;
+}
+
