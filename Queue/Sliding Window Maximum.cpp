7a58229c6be94b5661721d7fe40f91b10379b75d
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-07 21:10:38 +0530

    Update Sliding Window Maximum.cpp

--- Queue/Sliding Window Maximum.cpp
+++ Queue/Sliding Window Maximum.cpp
@@ -34,3 +34,34 @@ vector<int> Solution::slidingMaximum(const vector<int> &A, int B)
     }
     return ans;
 }
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Approach 2 : Optimal 
+
+Time Complexity: O(N)
+
+Space Complexity: O(K)
+    
+vector<int> Solution::slidingMaximum(const vector<int> &A, int k)
+{
+    deque<int> dq;
+    vector<int> ans;
+    for(int i=0; i<A.size(); i++)
+    {
+        if(!dq.empty() && dq.front() == i-k)
+        {
+            dq.pop_front();
+        }
+        while(!dq.empty() && A[dq.back()] < A[i])
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if(i >= k-1){
+            ans.push_back(A[dq.front()]);
+        }
+    }
+    return ans;
+}
+
