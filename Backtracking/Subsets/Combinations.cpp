d030dfdf4796809ecc5eebfe39b9eafbfe8b6deb
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-09-13 13:11:18 +0530

    Update Combinations.cpp

--- Backtracking/Subsets/Combinations.cpp
+++ Backtracking/Subsets/Combinations.cpp
@@ -1,4 +1,4 @@
-Time Complexity (TC): O(( B^A)) – generating all combinations of B elements from A. 
+Time Complexity (TC): O(( ACk)) – generating all combinations of B elements from A. 
 Space Complexity (SC): 𝑂 (𝐵) – depth of recursion and space to store temporary combinations.
 
 void helper(int index, int A, int B, vector<int> &temp, vector<vector<int>> &ans)
@@ -15,6 +15,32 @@ void helper(int index, int A, int B, vector<int> &temp, vector<vector<int>> &ans
     
     // exclude
     helper(index+1, A, B, temp, ans);
+}
+vector<vector<int> > Solution::combine(int A, int B) {
+    vector<vector<int>> ans;
+    vector<int> temp;
+    helper(1, A, B, temp, ans);
+    return ans;
+}
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    2nd Way
+
+    void helper(int index, int A, int B, vector<int> &temp, vector<vector<int>> &ans)
+{
+    if(B == 0){
+        ans.push_back(temp);
+        return;
+    }
+   // if (index > A) return;
+    
+    for(int i=index;i<=A; i++)
+    {
+        temp.push_back(i);
+        helper(i+1, A, B-1, temp, ans);
+        temp.pop_back();
+    }
 }
 vector<vector<int> > Solution::combine(int A, int B) {
     vector<vector<int>> ans;
